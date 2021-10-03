824dea24a47ae92cd89799187139bef4f14e5dc0
takuma0606 <reol1289@gmail.com>
2021-10-03 12:31:51 +0900

    ユーザー管理画面のメニューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'homes#top'
   get '/search' => 'searches#search'
+  get '/user' => 'users#user'
 end
